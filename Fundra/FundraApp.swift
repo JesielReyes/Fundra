1bebada111ccedb5bbcd22d56a4f9c92aab8fcde
Jesiel Reyes <144545348+JesielReyes@users.noreply.github.com>
2025-05-14 16:56:37 -0400

    Initial commit with Xcode project

---
+++ Fundra/FundraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundraApp.swift
+//  Fundra
+//
+//  Created by Jesiel Reyes on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FundraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FundraTests/FundraTests.swift
@@ -0,0 +1,16 @@
+//
+//  FundraTests.swift
+//  FundraTests
+//
+//  Created by Jesiel Reyes on 5/14/25.
+//
+
+import Testing
+
+struct FundraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
